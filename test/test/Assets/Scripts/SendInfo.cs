d704f4688258410e49a2e31951e6bdc53353cf46
freefire137 <ianawfinch@gmail.com>
2017-12-17 21:09:40 -0500

    according to youtube video

---
+++ test/test/Assets/Scripts/SendInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		bool RMB = Input.GetMouseButtonDown(1);
+		if (RMB) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Ground") {		//TODO
+				//this.GetComponent<PhotonView>().RPC("RecievedMove", PhotonTargets.All, hit.point);		move target using networking component
+			}
+		}
+
+
+	}
+}
